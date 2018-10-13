80b3ca7833d608f02e1ee2c9518ee4ce398e100f
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-10-14 00:37:07 +0600

    product route is changed

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get request to /products'
+    });
+});
