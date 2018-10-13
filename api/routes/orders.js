23a3d3c3450e5eff841a39b5088292ec06e3ead5
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-10-14 03:20:31 +0600

    get and delete method with ID is imported

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,7 +3,29 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: "You are ordered"
+        message: "You are GET ordered"
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: "You are POST ordered"
+    });
+});
+
+router.get('/:orderID', (req, res, next) => {
+    orderID = req.params.orderID;
+    res.status(200).json({
+        message: "You  GET with ID ordered",
+        ID: orderID
+    });
+});
+
+router.delete('/:orderID', (req, res, next) => {
+    orderID = req.params.orderID;
+    res.status(200).json({
+        message: "You are Deleted with ID ordered",
+        ID: orderID
     });
 });
 
