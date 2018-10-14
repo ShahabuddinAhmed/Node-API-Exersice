8be2d85aab1170901fa2b92139d63fb31574d45d
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-10-15 01:53:54 +0600

    double cotation is added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     res.status(200).json({
         message: "You are GET ordered"
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
     const order = {
         productID: req.body.productID,
         quantity: req.body.quantity
@@ -18,7 +18,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:orderID', (req, res, next) => {
+router.get("/:orderID", (req, res, next) => {
     orderID = req.params.orderID;
     res.status(200).json({
         message: "You  GET with ID ordered",
@@ -26,7 +26,7 @@ router.get('/:orderID', (req, res, next) => {
     });
 });
 
-router.delete('/:orderID', (req, res, next) => {
+router.delete("/:orderID", (req, res, next) => {
     orderID = req.params.orderID;
     res.status(200).json({
         message: "You are Deleted with ID ordered",
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,39 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     res.status(200).json({
-        message: 'Handling GET request to /products'
+        message: "Handling GET request to /products"
     });
 });
 
-router.post('/', (req, res, next) => {
-    const product = {
+router.post("/", (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
     res.status(201).json({
-        message: 'Handling POST request to /products',
+        message: "Handling POST request to /products",
         Product: product
     });
 });
 
-router.get('/:productID', (req, res, next) => {
+router.get("/:productID", (req, res, next) => {
     const id = req.params.productID;
-    if(id == 'spatial') {
+    if(id == "spatial") {
         res.status(200).json({
-            message: 'You discover the spatial ID',
+            message: "You discover the spatial ID",
             id: id
         });
     } else {
          res.status(200).json({
-            message: 'You pass an ID',
+            message: "You pass an ID",
             id: id
          });
     }
 });
 
-router.patch('/:productID', (req, res, next) => {
+router.patch("/:productID", (req, res, next) => {
     const id = req.params.productID;
     res.status(200).json({
         message: "You are Updated",
@@ -41,7 +50,7 @@ router.patch('/:productID', (req, res, next) => {
     });
 });
 
-router.delete('/:productID', (req, res, next) => {
+router.delete("/:productID", (req, res, next) => {
     const id = req.params.productID;
     res.status(200).json({
         message: "You are Deleted",
