3fad304773de5931b2cab1ef380a68cedec24331
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-10-15 18:44:28 +0600

    get method is added debugger

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,7 +8,13 @@ router.get("/", (req, res, next) => {
     .exec()
     .then(docs => {
         console.log(docs);
-        res.status(200).json(docs);
+        if(docs >= 0) {
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({
+                message: "No Data could not found"
+            });
+        }
     })
     .catch(err => {
         res.status(200).json({
